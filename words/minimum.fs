01c17b6c199c6659090beb2c24585d4f43efdcab
jeffmd <shelleydoyle@eastlink.ca>
2020-01-21 22:31:47 -0400

    minimum.fs converted to using register operations and is tested and working

--- words/minimum.fs
+++ words/minimum.fs
@@ -1,83 +1,50 @@
 \ minimum.fs Forth words that make up minimum forth vocabulary
 
-
-( n min max -- f)
 \ check if n is within min..max
 \ flag is 1 if min <= n <= max
-: within
-    popy       ( n max Y:min )
-    -=y        ( n diff )
-    x=d0 x-=y  ( n diff X:n-min )
-    d0=x       ( n-min diff ) 
-    u<         ( flag )
+: within ( n min max -- f)
+  y=d        ( n max Y:min )
+  x=d0       ( n max Y:min X:n )
+  -y         ( n diff )
+  x-y        ( n diff X:n-min )
+  d0=x       ( n-min diff ) 
+  u<         ( flag )
 ;
 
 \ increment a cvar by one.  If val > max then set flag to true.
 : 1+c@mx ( maxval cvar -- flag )
-  popy      ( cvar Y:maxval ) 
+  y=d       ( cvar Y:maxval ) 
   x= c@x    ( val X:cvar )
-  1+ push   ( val+1 val+1 )
+  +1 d=     ( val+1 val+1 )
   y >       ( val+1 flag )
   ==0
   ifnz 0 then
-  push c@x= 0=
+  d= c@x= 0=
 ;
-
-\ signed multiply n1 * n2 and division  with n3 with double
-\ precision intermediate and remainder
-: */mod  ( n1 n2 n3 -- rem quot )
-    >r
-    m*
-    r>
-    m/mod
-;
-
-
-\ signed multiply and division with double precision intermediate
-: */ ( n1 n2 n3 -- n4 )
-    */mod
-    nip
-;
-
 
 \ divide n1 by n2 giving the remainder n3
 : mod ( n1 n2 -- n3 )
-    /mod
-    drop
+  /mod =d
 ;
-
-
-\ fill u bytes memory beginning at a-addr with character c
-\ : fill  ( a-addr u c -- )
-\    -rot           ( c a-addr u )
-\    nip>a          ( c u ) ( A: a-addr )
-\    begin
-\    ?while
-\      over         ( c u c )
-\      ac!          ( c u )
-\      a+
-\      1-           ( c u-1 )
-\    repeat
-\    2drop
-\ ;
-
 
 \ emits a space (bl)
 : space ( -- )
-    bl emit
+  bl emit
 ;
 
 \ emits n space(s) (bl)
 \ only accepts positive values
 : spaces ( n -- )
-    \ make sure a positive number
-    dup 0> and
-    begin
-    ?while
-      space
-      1-
-    repeat
-    drop
+  \ make sure a positive number
+  y= >0 &y
+  d=
+  ==0
+  begin
+  whilenz
+    space
+    d0 1- d0=
+  repeat
+  d-1
 ;
 
 \ pointer to current write position
@@ -87,141 +54,129 @@ var hld
 
 \ prepend character to pictured numeric output buffer
 : hold ( c -- )
-    -1 hld +!
-    hld @ c!
+  x= hld y=
+  @y 1- @y=  
+  c@=x
 ;
 
 \ Address of the temporary scratch buffer.
 : pad ( -- a-addr )
-    here 100 +
+    here y# 100 +y
 ;
 
 \ initialize the pictured numeric output conversion process
 : <# ( -- )
-    pad hld !
+    d= pad y= hld @=y =d
 ;
 
 \ pictured numeric output: convert one digit
 : # ( u1 -- u2 )
-    base@      ( u1 base )
-    u/mod      ( rem u2 )
-    swap       ( u2 rem )
-    #h hold    ( u2 )
+  d= base    ( u1 base )
+  /mod       ( rem u2 )
+  y=d0 d0= y ( u2 rem )
+  #h hold =d ( u2 )
 ;
 
 \ pictured numeric output: convert all digits until 0 (zero) is reached
 : #s ( u -- 0 )
+  #
+  begin
+  ==0
+  whilenz
     #
-    begin
-    ?while
-      #
-    repeat
+  repeat
 ;
 
 \ copy string to numeric output buffer
 : #$ ( addr len -- )
 \ start at end of string
-    begin
-    ?while
-      1- over over + c@ hold
-    repeat
-    2drop    
+  d=        ( addr len len )
+  begin
+  d0
+  ==0
+  whilenz
+    1- d0= y=
+    d1 +y c@ hold
+  repeat
+  d-1    
 ;
 
 \ Pictured Numeric Output: convert PNO buffer into an string
 : #> ( u1 -- addr count )
-    drop hld @ pad over -
+  hld @ d= x=  ( addr addr Y:addr ) 
+  pad -x       ( addr count )
 ;
 
 \ place a - in HLD if n is negative
 : sign ( n -- )
-    0< if [char] - hold then
+    <0 ==0 ifnz [char] - hold then
 ;
 
 
 \ singed PNO with cell numbers, right aligned in width w
 : .r ( wantsign n w -- )
-    >r   ( wantsign n ) ( R: w )
-    <#
-    #s   ( wantsign 0 )
-    swap ( 0 wantsign )
-    sign ( 0 )
-    #>   ( addr len )
-    r>   ( addr len w )  ( R: )
-    over ( addr len w len )
-    -    ( addr len spaces )
-    spaces ( addr len )
-    type  ( )
-    space
+  r= =d   ( wantsign n ) ( R: w )
+  <# #s   ( wantsign 0 )
+  =d sign ( ? )
+  #>      ( addr len )
+  d= =r   ( addr len w )  ( R: )
+  y=d0    ( addr len w Y:len )
+  -y      ( addr len spaces )
+  spaces  ( addr len ? )
+  =d type ( )
+  space
 ;
 
 \ unsigned PNO with single cell numbers
 : u. ( u -- )
-    0      ( n 0 ) \ want unsigned
-    tuck   ( 0 n 0 )
-    .r
+  d= d= 0 ( n 0 ) \ want unsigned
+  d1=     ( 0 n 0 )
+  .r
 ;
 
 
 \ singed PNO with single cell numbers
 : .  ( n -- )
-    dup      ( n n )
-    abs      ( n n' )
-    0        ( n n' 0 ) \ not right aligned
-    .r
+  d=       ( n n )
+  abs      ( n n' )
+  d= 0     ( n n' 0 ) \ not right aligned
+  .r
 ;
 
 \ stack dump
+\ prints stack contents from bottom to top with
+\ working register printed las
 : .s  ( -- )
-    sp@         ( limit ) \ setup limit
-    dcell-
-    sp0         ( limit counter )
-    begin
-      dcell-    ( limit counter-4 )
-      2over     ( limit counter-4 limit counter-4 )
-      <>        ( limit counter-4 flag )
-      while
-        dup     ( limit counter-4 counter-4 )
-        @       ( limit counter-4 val )
-        u.      ( limit counter-4 )
-    repeat
-    2drop
+  d=          ( ? ? )
+  dsp         ( ? limit ) \ setup limit
+  dcell-
+  d= dsp0     ( ? limit counter )
+  begin
+    dcell-    ( ? limit counter-4 )
+    y=d0 d=   ( ? limit counter-4 counter-4 Y:limit  )
+    d=y       ( ? limit counter-4 limit counter-4 )
+    !=        ( ? limit counter-4 flag )
+    ==0
+  whilenz
+    d0        ( ? limit counter-4 counter-4 )
+    @         ( ? limit counter-4 val )
+    . =d      ( ? limit counter-4 )
+  repeat
+  d-2 =d
 ;
-
-\ numbers that get used a lot
-
-: 3 3 ;
-: 5 5 ;
-: 6 6 ;
-: 7 7 ;
-: 8 8 ;
-: 9 9 ;
-: $FF $FF ;
-: $F0 $F0 ;
-: $0F $0F ;
-: $FF00 $FF00 ;
-
-
-( xt1 c<char> -- )
-\ stores xt into defer or compiles code to do so at runtime
-\ : is
-\    [compile] to
-\ ; immediate
 
 \ 1 millisecond delay
 ( -- )
 : 1ms 1000 usleep ;
 
-
 \ set name as the background task that
 \ executes each time pause executes
 ( char:name -- )
-: dopause ' pause# ! ;
+: dopause ' pause= ;
 
 \ put a null at end of string
-: $_ ( addr len -- )
-
- \ addr + len = 0
- 1- + 0c!
+: $_ ( addr len -- addr )
+  \ [addr + len-1] = 0
+  y=d x= y+ y-1 0 c@y= x
 ;
 
