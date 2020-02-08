9e4ae1c7c0ee7682469b2e4419e12d31621bc45e
jeffmd <shelleydoyle@eastlink.ca>
2020-02-07 20:02:20 -0400

    gpio.fs: now using register operations, tested and working.

--- words/gpio.fs
+++ words/gpio.fs
@@ -1,7 +1,7 @@
 \ gpio.fs - words to manipulate gpio pins on Raspberry Pi
 only
-vocabulary GPIO
-GPIO definitions
+voc: GPIO
+GPIO def
 
 : root$ s" /sys/class/gpio/" ;
 : exp$ s" export" ;
@@ -9,129 +9,152 @@ GPIO definitions
 : dir$ s" /direction" ;
 : value$ s" /value" ;
 : gpio$ s" gpio" ;
-: inout$ s" inout" ;
+: in$ s" in" ;
+: out$ s" out" ;
 : inbuf$ s"    " ;
 
 : .fail ." Failed to open GPIO: " ;
 : .wrting ."  for writing!" ;
 
-\ start null terminated buffer
-: <#_ ( -- )
-    <# 0 hold
+\ file open using null terminated buffer
+: #>Open ( mode straddr len -- fs flag )
+  #$ root$ #$ #>    ( mode addr len )
+  =d open           ( fs )
+  d= >0             ( fs flag )
 ;
 
-\ file open using null terminated buffer
-: #>Open ( mode straddr len -- fs flag)
-    #$ root$ #$ #> drop open ( fs )
-    dup 0> ( fs flag )
+\ write pin number to file
+: writePin ( fs pin -- fs flag )
+  <# #s #>       ( fs addr len )
+  write          ( fs flag )
 ;
 
 \ unexport a GPIO pin when no longer needed by user
 : unexp ( pin --  )
-    \ open unexport file for writing
-    1 dup  ( pin 1 1 )
-    <#_ unexp$ #>Open ( pin fs flag)
-    if
-      dup ( pin fs fs )
-      rot ( fs fs pin )
-      <# #s #> write ( fs flag )
-      swap
-    else
-      .fail unexp$ type .wrting
-    then
-    nip
-    close
+  \ open unexport file for writing
+  d= 1                ( pin 1 )
+  <#_ unexp$ #>Open   ( pin fs flag)
+  ==0
+  ifnz
+    d1                ( pin fs pin )
+    writePin          ( pin fs flag )
+    d0 close          ( pin fs ? )
+  else
+    .fail unexp$ type
+    .wrting           ( pin fs ? )
+  then
+  d-2                 ( ? )
 ;
 
 \ open the direction file
 : dirOpen ( pin -- fs flag )
-    1    ( pin 1 )
-    swap  ( 1 pin )
-    <#_ dir$ #$ #s gpio$ #>Open ( fs flag)
+    y# 1 d=y       ( 1 pin Y:1 )
+    <#_ dir$ #$    ( 1 pin ? )
+    =d #s          ( 1 0 )
+    gpio$ #>Open   ( fs flag)
+;
+
+\ wait for direction file to become available
+: waitDirFile ( pin fs ? -- pin fs ? )
+  begin
+    d1           ( pin fs pin ) 
+    dirOpen      ( pin fs fs2 flag )
+    ==0
+    =d           ( pin fs fs2 )
+  untilnz
+  close          ( pin fs ? )
 ;
 
 \ export a pin - prepare for gpio use
 : exp ( pin -- )
-    1 dup            ( pin 1 1 )
-    <#_ exp$ #>Open  ( pin fs flag )
-    if
-      swap           ( fs pin )
-      over           ( fs pin fs )
-      over           ( fs pin fs pin )
-      <# #s #> write ( fs pin 1 )
-      rot close      ( pin 1 )
-      \ wait for direction file to become available
-      begin
-        drop         ( pin )
-        dup          ( pin pin ) 
-        dirOpen      ( pin fs flag )
-      until
-    else
-      .fail exp$ type .wrting
-    then
-    nip
-    close
+  d= 1             ( pin 1 )
+  <#_ exp$ #>Open  ( pin fs flag )
+  ==0 
+  ifnz
+    d1             ( pin fs pin )
+    writePin       ( pin fs flag )
+    d0 close       ( pin fs ? )
+    waitDirFile    ( pin fs ? )
+  else
+    .fail exp$ type .wrting
+  then
+  d-2              ( ? )
+;
+
+\ select direction string
+\ direction  string
+\    0         in
+\    1        out
+: selDir$ ( direction -- addr len )
+  ==0
+  ifz
+    in$     ( addr len )
+  else
+    out$    ( addr len )
+  then
 ;
 
 \ set direction of a GPIO pin that has been exported
 \ if direction is 0 then pin will be input
 \ if direction is 1 then pin will be output
 : dir ( pin direction --  )
-    \ open direction file for writing
-    swap ( direction pin )
-    dirOpen    ( direction fs flag )
-    if
-      dup ( direction fs fs )
-      rot ( fs fs direction )
-      <# dup 2* inout$ drop + over 2+ #$ #> write ( fs flag )
-      swap
-    else
-      .fail ." direction" .wrting
-    then
-    nip
-    close
+  \ open direction file for writing
+  d= d1          ( pin direction pin )
+  dirOpen        ( pin direction fs flag )
+  ==0 
+  ifnz
+    d1           ( pin direction fs direction )
+    selDir$      ( pin direction fs addr len )
+    write        ( pin direction fs flag )
+    d0 close     ( pin direction fs ? )
+  else
+    .fail ." direction" .wrting
+  then
+  d-3            ( pin direction fs ? )
 ;
 
 \ Open gpio value file
 \ mode: 0 file opened for reading
 \ mode: 1 file opened for writing
-: valOpen ( pin mode -- fs flag )
-    \ open value file for writing
-    swap ( mode pin )
-    <#_ value$ #$ #s gpio$ #>Open ( fs flag)
+: valOpen ( mode pin -- fs flag )
+  \ open value file for writing
+  <#_ value$ #$  ( mode pin ? )
+  =d #s          ( mode 0 )
+  gpio$ #>Open   ( fs flag)
 ;
 
 \ write to gpio pin
 : pinW ( pin val --  )
-    swap 1 valOpen ( val fs flag )
-    if
-      dup ( val fs fs )
-      rot ( fs fs val )
-      <# #s #> write ( fs 1 )
-      swap
-    else
-      .fail ." value" .wrting
-    then
-    nip
-    close
+  y=d0 d0= 1     ( val 1 Y:pin )
+  d= y           ( val 1 pin )
+  valOpen        ( val fs flag )
+  ==0
+  ifnz
+    d1           ( val fs val )
+    writePin     ( val fs 1 )
+  else
+    .fail ." value" .wrting
+  then
+  d0             ( val fs fs )
+  close          ( val fs ? )
+  d-2            ( ? )
 ;
 
 \ read from gpio pin
 : pinR ( pin -- val )
-    0 valOpen ( fs flag )
-    if
-      dup ( fs fs )
-      inbuf$ read ( fs flag )
-      \ convert string val to integer
-      inbuf$ drop 1 num drop ( fs flag val )
-      -rot ( val fs flag )
-      swap
-    else
-      .fail ." value for reading!"
-      tuck ( flag fs flag )
-    then
-    nip
-    close
+  y=0 d=y        ( 0 pin ) 
+  valOpen        ( fs flag )
+  ==0
+  ifnz
+    inbuf$ read  ( fs buffer count )
+    \ convert string val to integer
+    1 num        ( fs val flag )
+    d1 close     ( fs val ? )
+    =d           ( fs val )
+  else
+    .fail ." value for reading!"
+  then
+  d-1            ( val )
 ;
 
 \ setup a GPIO pin for input or output
@@ -139,26 +162,28 @@ GPIO definitions
 \ if direction is 0 then pin will be input
 \ if direction is 1 then pin will be output
 : pin ( pin direction -- )
-  over exp dir
+  d= d1    ( pin direction pin ) 
+  exp      ( pin direction ? )
+  =d dir   ( ? )
 ;
 
 \ set pin as output
 : output ( pin -- )
-  1 pin
+  d= 1 pin
 ;
 
 \ set pin as input
 : input ( pin -- )
-  0 pin
+  d= 0 pin
 ;
 
 \ turn gpio pin to high state
 : high ( pin -- )
-  1 pinW
+  d= 1 pinW
 ;
 
 \ turn gpio pin to low state
 : low ( pin -- )
-  0 pinW
+  d= 0 pinW
 ;
 
