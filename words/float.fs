0f352de8f7935698549e5fb5457f81d4bd59389d
jeffmd <shelleydoyle@eastlink.ca>
2020-02-19 11:21:30 -0400

    BugFix: .f prints malformed decimal portion - new words: pi rad

--- words/float.fs
+++ words/float.fs
@@ -1,15 +1,28 @@
 \ float.fs words that deal with floating point numbers
 
 \ print floating point number
+\ no exponent support
 : .f ( f -- )
   d= i d=        ( f i i ) 
   ..             ( f i ? )
-  ." ."
+  [char] . emit
   d0 f x=        ( f i nf X:nf )
   d1 f-          ( f i f-nf )
-  d= 10000 f x=  ( f i fr 10000.0f X:10000.0f )
-  =d f* i        ( f i fr*10000.0f )
-  abs .          ( f i ? )
+  d= base x=
+  *x *x *x *x *x *x f
+  x=             ( f i fr  X:10000000.0f )
+  =d f* i        ( f i fr*10000000.0f )
+  abs            ( f i n )
+  <# # # # # # # # #>
+  type space
   d-2            ( ? )
 ;
 
+: pi ( -- 3.141592 )
+  3.14159265
+;
+
+\ convert float angle in degrees to radians
+: rad ( deg -- rad )
+  x# 180. f/ x= pi f*
+;
