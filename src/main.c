647656fa7e8306a258000a05806e51229fa3a6e8
jeffmd <shelleydoyle@eastlink.ca>
2021-02-09 17:59:08 -0400

    if: BugFix: new code no longer executable - code placed in .bss is no longer defaults to executable, only read/write - make new code placed in .bss cp section executable on program start

--- src/main.c
+++ src/main.c
@@ -7,15 +7,19 @@
 #include <string.h>
 #include <signal.h>
 #include <setjmp.h>
+#include <sys/mman.h>
 
 #include "input.h"
+
+#define CODE_SIZE 0xffff
 
 extern void COLD(void);
 extern void RECOVER(void);
 extern int USER_ARGC;
 extern char **USER_ARGV;
+extern int CPSTART;
 
-sigjmp_buf quitbuf;
+static sigjmp_buf quitbuf;
 
 void handler (int signo, siginfo_t *info, void *arg)
 {
@@ -23,6 +27,15 @@ void handler (int signo, siginfo_t *info, void *arg)
   printf("Faulted at address 0x%x\n", info->si_addr);
   
   siglongjmp( quitbuf, -1 );
+}
+
+void make_cp_memory_executable(void)
+{
+  int status = mprotect(&CPSTART, CODE_SIZE, PROT_READ | PROT_WRITE | PROT_EXEC);
+
+  if (status) {
+    perror("mprotect new code segment");
+  }
 }
 
 int main(int argc, char *argv[])
@@ -37,6 +50,7 @@ int main(int argc, char *argv[])
   USER_ARGC = argc;
   USER_ARGV = argv;
 
+  make_cp_memory_executable();
   set_input_mode();
   
   if (sigsetjmp(quitbuf, 1) != 0)
@@ -47,6 +61,6 @@ int main(int argc, char *argv[])
   {
     COLD();
   }
-  
+
   return 0;
 }
